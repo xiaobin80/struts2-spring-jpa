b5c7318dd7f76ef102c9f4194ece0cc4e2c02bdb
xiaobin <tdtc_hrb@163.com>
2018-05-01 22:16:18 +0800

    Enabled Simple Spring Memcached

--- src/main/java/com/example/demo/service/TrainOrderService.java
+++ src/main/java/com/example/demo/service/TrainOrderService.java
@@ -3,7 +3,8 @@ package com.example.demo.service;
 import java.util.List;
 
 import com.example.demo.model.TrainOrder;
+import com.google.code.ssm.api.ParameterValueKeyProvider;
 
 public interface TrainOrderService {
-	List<TrainOrder> getCarList(String year, String month, int trainNumber);
+	List<TrainOrder> getCarList(String year, String month, @ParameterValueKeyProvider int trainNumber);
 }
--- src/main/java/com/example/demo/service/TrainOrderServiceImpl.java
+++ src/main/java/com/example/demo/service/TrainOrderServiceImpl.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.TrainOrderDao;
 import com.example.demo.model.TrainOrder;
+import com.google.code.ssm.api.ParameterValueKeyProvider;
+import com.google.code.ssm.api.ReadThroughSingleCache;
 
 @Service("trainOrderService")
 public class TrainOrderServiceImpl implements TrainOrderService {
 
 	@Autowired
 	TrainOrderDao trainOrderDao;
+	
 	@Override
-	public List<TrainOrder> getCarList(String year, String month, int trainNumber) {
+	@ReadThroughSingleCache(namespace = "carnum", expiration = 300)
+	public List<TrainOrder> getCarList(String year, String month, @ParameterValueKeyProvider int trainNumber) {
 		// TODO Auto-generated method stub
 		return trainOrderDao.getCarnumberList(year, month, trainNumber);
 	}
